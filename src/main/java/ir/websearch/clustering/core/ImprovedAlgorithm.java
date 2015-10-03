1c171101005e01cdd1b84da3cb269410d4582431
Asher Gruber <asher@legolas-media.com>
2015-10-03 15:32:13 +0300

    Added the Stanford Lemmatizer to the improved algorithm.

--- src/main/java/ir/websearch/clustering/core/ImprovedAlgorithm.java
+++ src/main/java/ir/websearch/clustering/core/ImprovedAlgorithm.java
@@ -3,11 +3,26 @@ package ir.websearch.clustering.core;
 import java.util.Collection;
 
 import ir.websearch.clustering.doc.Document;
+import ir.websearch.clustering.helper.StanfordLemmatizer;
 
 public class ImprovedAlgorithm extends BasicAlgorithm {
+	
+	private static final double CLUSTER_CLASSIFICATION_THRESHOLD = 0.0001;
+	StanfordLemmatizer slem = new StanfordLemmatizer();
 
 	public ImprovedAlgorithm(Collection<Document> docs) {
 		super(docs);
+	}
+	
+	@Override
+	protected double getClusterClassificationThreshold() {
+		return CLUSTER_CLASSIFICATION_THRESHOLD;
+	}
+	
+	@Override
+	protected String getDocumnetText(Document doc) {
+		String lemmatizedText = slem.lemmatize(doc.getText());
+		return lemmatizedText;
 	}
 	
 }
